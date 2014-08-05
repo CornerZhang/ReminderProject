03243770f61edef8a3b792c86e7f15bca2f87b3d
CornerZhang <cyberzei@qq.com>
2014-08-05 17:11:40 +0800

    * 更改RooView为PageView，确认iPhone/iPad模拟器运行后的，iPad在横过来时，左右两页正确显示 。

---
+++ Reminder_restarter/Reminder_restarter/NXPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  NXPageViewController.h
+//  Reminder_restarter
+//
+//  Created by CornerZhang on 14-8-4.
+//  Copyright (c) 2014年 NeXtreme.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NXPageViewController : UIPageViewController <UIPageViewControllerDelegate>
+
+@end
---
+++ Reminder_restarter/Reminder_restarter/NXPageViewController.m
@@ -0,0 +1,97 @@
+//
+//  NXPageViewController.m
+//  Reminder_restarter
+//
+//  Created by CornerZhang on 14-8-4.
+//  Copyright (c) 2014年 NeXtreme.com. All rights reserved.
+//
+
+#import "NXPageViewController.h"
+#import "NXModelController.h"
+#import "NXDataViewController.h"
+
+@interface NXPageViewController ()
+@property (readonly, strong, nonatomic) NXModelController *modelController;
+@end
+
+@implementation NXPageViewController
+
+@synthesize modelController = _modelController;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    // Configure the page view controller and add it as a child view controller.
+
+    _modelController = [[NXModelController alloc] init];
+    
+    self.delegate = self;
+
+    self.dataSource = self.modelController;
+
+    // Set the page view controller's bounds using an inset rect so that self's view is visible around the edges of the pages.
+    CGRect pageViewRect = self.view.bounds;
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {	// for iPad
+        pageViewRect = CGRectInset(pageViewRect, 40.0, 40.0);
+    }
+    self.view.frame = pageViewRect;
+
+    NXDataViewController *startingViewController = [self.modelController viewControllerAtIndex:0 storyboard:self.storyboard];
+    NSArray *viewControllers = @[startingViewController];
+    [self setViewControllers:viewControllers
+                   direction:UIPageViewControllerNavigationDirectionForward
+                    animated:NO
+                  completion:nil];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - UIPageViewController delegate methods
+
+/*
+- (void)pageViewController:(UIPageViewController *)pageViewController didFinishAnimating:(BOOL)finished previousViewControllers:(NSArray *)previousViewControllers transitionCompleted:(BOOL)completed
+{
+    
+}
+ */
+
+- (UIPageViewControllerSpineLocation)pageViewController:(UIPageViewController *)pageViewController
+                   spineLocationForInterfaceOrientation:(UIInterfaceOrientation)orientation
+{
+    if (UIInterfaceOrientationIsPortrait(orientation) || ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone)) {
+        // In portrait orientation or on iPhone: Set the spine position to "min" and the page view controller's view controllers array to contain just one view controller. Setting the spine position to 'UIPageViewControllerSpineLocationMid' in landscape orientation sets the doubleSided property to YES, so set it to NO here.
+        
+        UIViewController *currentViewController = self.viewControllers[0];
+        NSArray *viewControllers = @[currentViewController];
+        [self setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionForward animated:YES completion:nil];
+        
+        self.doubleSided = NO;
+        return UIPageViewControllerSpineLocationMin;
+    }
+
+    // In landscape orientation: Set set the spine location to "mid" and the page view controller's view controllers array to contain two view controllers. If the current page is even, set it to contain the current and next view controllers; if it is odd, set the array to contain the previous and current view controllers.
+    NXDataViewController *currentViewController = self.viewControllers[0];
+    NSArray *viewControllers = nil;
+
+    NSUInteger indexOfCurrentViewController = [self.modelController indexOfViewController:currentViewController];
+    if (indexOfCurrentViewController == 0 || indexOfCurrentViewController % 2 == 0) {
+        UIViewController *nextViewController = [self.modelController pageViewController:self viewControllerAfterViewController:currentViewController];
+        viewControllers = @[currentViewController, nextViewController];
+    } else {
+        UIViewController *previousViewController = [self.modelController pageViewController:self viewControllerBeforeViewController:currentViewController];
+        viewControllers = @[previousViewController, currentViewController];
+    }
+    
+    [self setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionForward animated:YES completion:nil];
+
+
+    return UIPageViewControllerSpineLocationMid;
+}
+
+@end
